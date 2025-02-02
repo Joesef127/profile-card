7ae1a4af6914a40b6c012f856fac030eb467c84a
Adedayo Adegbola <adegboladayor@gmail.com>
2025-02-02 12:01:26 +0100

    chore: configured timestamp and added more icons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function updateTime() {
-  const utcTime = new Date().toUTCString();
-  document.getElementById('utc-time').innerText = `UTC Time: ${utcTime}`;
+  const timestamp = Date.now();
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  }
+  const currentTime = new Date(timestamp).toLocaleTimeString('en-US', options);
+  document.getElementById('utc-time').innerText = currentTime
 }
 updateTime();
 setInterval(updateTime, 1000);
